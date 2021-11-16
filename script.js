69253f28a96778a19d56e39e5027159869fb843c
Elegis Sossou <elegis.sossou@yahoo.com>
2021-11-16 16:15:46 +0100

    Adding moving arrow to the hero bottom

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 //Change navbar expanded bg color
-var navbar = document.getElementsByClassName("navbar navbar-expand-sm fixed-top")[0];
+var navbar = document.getElementsByClassName("navbar navbar-expand-md fixed-top")[0];
 var toggleButton = document.getElementsByClassName("navbar-toggler")[0];
 toggleButton.addEventListener('click', ()=> {
     navbar.classList.toggle("nav-expanded");
