61a7d241747bc25efb60acc87b32e520fbd444e9
Kevsely <elegis.sossou@yahoo.com>
2021-12-08 15:40:31 +0100

    Adding vertical slider as testimony

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-//Making navbar opaque while scrolling
-
-
 //Change navbar expanded bg color
 var navbar = document.getElementsByClassName("navbar navbar-expand-md fixed-top")[0];
 var toggleButton = document.getElementsByClassName("navbar-toggler")[0];
@@ -9,7 +6,34 @@ toggleButton.addEventListener('click', ()=> {
     console.log('click');
 })
 
-
 //Panoramic Diaporama
 var nextButton = document.getElementsByClassName("carousel-control-next revolution-next")[0];
 setInterval(()=>nextButton.click(), 20000);
+
+//Testimony vertical slider
+var cards = Array.from(document.querySelectorAll('#card-slider .slider-item'));
+startAnim(cards);
+
+function startAnim(array){
+    if(array.length >= 4 ) {
+        TweenMax.fromTo(array[0], 0.5, {x:0, y: 0, opacity:0.75}, {x:0, y: -120, opacity:0, zIndex: 0, delay:0.03, ease: Cubic.easeInOut, onComplete: sortArray(array)});
+
+        TweenMax.fromTo(array[1], 0.5, {x:79, y: 125, opacity:1, zIndex: 1}, {x:0, y: 0, opacity:0.75, zIndex: 0, boxShadow: '-5px 8px 8px 0 rgba(82,89,129,0.05)', ease: Cubic.easeInOut});
+
+        TweenMax.to(array[2], 0.5, {bezier:[{x:0, y:250}, {x:65, y:200}, {x:79, y:125}], boxShadow: '-5px 8px 8px 0 rgba(82,89,129,0.05)', zIndex: 1, opacity: 1, ease: Cubic.easeInOut});
+
+        TweenMax.fromTo(array[3], 0.5, {x:0, y:400, opacity: 0, zIndex: 0}, {x:0, y:250, opacity: 0.75, zIndex: 0, ease: Cubic.easeInOut}, );
+    } else {
+        document.getElementById('card-slider').append('<p>Sorry, carousel should contain more than 3 slides</p>')
+    }
+}
+
+function sortArray(array) {
+    clearTimeout(delay);
+    var delay = setTimeout(function(){
+        var firstElem = array.shift();
+        array.push(firstElem);
+        return startAnim(array); 
+    },3000)
+}
+
